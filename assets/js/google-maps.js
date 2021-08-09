8004a35691ae82b5f2ec1fc3e4e5f888d2613e02
dulminihettiarachchi <dulminid1234@gmail.com>
2021-08-09 16:19:44 +0530

    fourth commit

--- assets/js/google-maps.js
+++ assets/js/google-maps.js
@@ -1,6 +1,6 @@
 function initMap() {
     // Latitude and Longitude
-    var myLatLng = {lat: 6.8566791, lng: 80.2233214};
+    var myLatLng = {lat: 6.856591, lng: 80.2481894};
 
     var map = new google.maps.Map(document.getElementById('google-maps'), {
         zoom: 17,
